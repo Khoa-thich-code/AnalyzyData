0652eb805c8af5e092fbe9423820f9d5743ad5ee
Khoa-thich-code <khoauranium@gmail.com>
2023-10-20 23:56:33 +0700

    Analyze Input String

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import controller.AnalyzeInputString;
+
+public class Main {
+
+    public static void main(String[] args) {   
+        new AnalyzeInputString().run();
+    }
+    
+}
---
+++ src/main/java/model/Data.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Data {
+    private String inputString;
+
+    public Data() {
+    }
+
+    public Data(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+    
+    
+}
